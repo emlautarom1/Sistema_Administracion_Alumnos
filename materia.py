3258ba794419e9326558c7244a956cdee104d339
lautaroem1 <emlautarom1@gmail.com>
2018-11-16 01:57:22 -0300

    Private attributes in materia

--- materia.py
+++ materia.py
@@ -1,30 +1,30 @@
 class Materia:
 
     def __init__(self, nombre='none', nro_reg=0):
-        self.nombre = nombre
-        self.nro_reg = nro_reg
-        self.notas = [0, 0, 0]
+        self.__nombre = nombre
+        self.__nro_reg = nro_reg
+        self.__notas = [0, 0, 0]
 
     def get_nombre(self):
-        return self.nombre
+        return self.__nombre
 
     def get_nro_reg(self):
-        return self.nro_reg
+        return self.__nro_reg
 
     def get_notas(self):
-        return self.notas
+        return self.__notas
 
     def set_notas(self, notas: list):
-        self.notas = notas
+        self.__notas = notas
 
     def set_nota_trimestre(self, trimestre: int, nota: int):
         if -1 < trimestre < 3:
-            self.notas[trimestre] = nota
+            self.__notas[trimestre] = nota
         else:
             raise ValueError
 
     def get_nota_trimestre(self, trimestre: int):
         if -1 < trimestre < 3:
-            return self.notas[trimestre]
+            return self.__notas[trimestre]
         else:
             raise ValueError
