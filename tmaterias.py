42d9cb480b8dd2a441260bd17116311fa6988782
lautaroem1 <emlautarom1@gmail.com>
2018-11-16 01:59:08 -0300

    Private attributes

--- tmaterias.py
+++ tmaterias.py
@@ -5,8 +5,8 @@ from bisect import insort
 
 class TMaterias:
     def __init__(self):
-        self.listado = []
-        self.key_materias = [
+        self.__listado = []
+        self.__key_materias = [
             'Matematica',
             'Lengua',
             'Fisica',
@@ -19,46 +19,46 @@ class TMaterias:
         ]
 
     def alta(self, mat: Materia):
-        if mat.get_nombre() not in self.key_materias:
+        if mat.get_nombre() not in self.__key_materias:
             raise KeyError('El nombre de la materia es inválida.')
         else:
-            for val in self.listado:
+            for val in self.__listado:
                 if mat.get_nombre() == val.get_nombre() and mat.get_nro_reg() == val.get_nro_reg():
                     raise KeyError('Ya existe la materia.')
-            self.listado.append(mat)
+            self.__listado.append(mat)
 
     def baja(self, nro_reg: int, nombre: str):
-        for val in self.listado:
+        for val in self.__listado:
             if nro_reg == val.get_nro_reg() and nombre == val.get_nombre():
-                self.listado.remove(val)
+                self.__listado.remove(val)
                 break
         else:
             raise KeyError('La materia no se encuentra.')
 
     def baja_total(self, nro_reg: int):
         to_remove = []
-        for val in self.listado:
+        for val in self.__listado:
             if val.get_nro_reg() == nro_reg:
                 to_remove.append(val)
         for val in to_remove:
-            self.listado.remove(val)
+            self.__listado.remove(val)
 
     def modificar(self, mat: Materia):
         self.baja(mat.get_nro_reg(), mat.get_nombre())
         self.alta(mat)
 
     def consulta(self, nro_reg: int, nombre: str):
-        if nombre not in self.key_materias:
+        if nombre not in self.__key_materias:
             raise KeyError('El nombre de la materia es inválida.')
         else:
-            for val in self.listado:
+            for val in self.__listado:
                 if nro_reg == val.get_nro_reg()and nombre == val.get_nombre():
                     return val
             else:
                 raise KeyError('La materia no se encuentra.')
 
     def get_materias(self):
-        return self.listado
+        return self.__listado
 
     def get_materias_alumno(self, nro_reg: int):
-        return list(filter(lambda m: m.get_nro_reg() == nro_reg, self.listado))
+        return list(filter(lambda m: m.get_nro_reg() == nro_reg, self.__listado))
